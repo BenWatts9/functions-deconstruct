f77ce9e834f540df99d9a9affc14b31ebfdc4856
BenWatts9 <brandolphwatts@gmail.com>
2022-02-17 11:23:12 -0600

    initial commit for 3rd functions exercise

---
+++ main.js
@@ -0,0 +1,54 @@
+//1
+const grabBoard = () => {
+    console.log( '1. Grab Skateboard!')
+}
+//2
+const getGear = () => {
+    console.log('2. Grab helmet, shoes and misc equipment.')
+}
+//3
+const getCar = () => {
+    console.log('3. Get in car.')
+}
+//4
+const driveToPark = () => {
+    console.log('4. Drive to skatepark.')
+}
+//5
+const unload = () => {
+    console.log('5. Unload skateboard and gear.')
+}
+//6
+const shred = () => {
+    console.log('6. Do some tricks and try to not run over anyone.')
+}
+//7
+const probablyFall = () => {
+    console.log('7. Probably fall down while trying a trick thats too hard.')
+}
+//8
+const getHurt = () => {
+    console.log('8. Probably get hurt because of the fall')
+}
+//9
+const haveBlast = () => {
+    console.log('9. Still have a great time anyway.')
+}
+const comeHome = () => {
+    console.log('10. Leave the park and come home.')
+}
+const beSad = () => {
+    console.log('11. Be sad now cause you are not skating.')
+}
+
+grabBoard()
+getGear()
+getCar()
+driveToPark()
+unload()
+shred()
+probablyFall()
+getHurt()
+haveBlast()
+comeHome()
+beSad()
